a6bb9b64b4b6dd8837d345fa90ad6e6163ce63b3
Jaghadeep-345 <140903648+Jaghadeep@users.noreply.github.com>
2026-01-28 00:30:56 +0530

    Add files via upload

---
+++ EX 2/EX 2.py
@@ -0,0 +1,13 @@
+import os
+from dotenv import load_dotenv
+from langchain_groq import ChatGroq
+
+load_dotenv()
+
+llm = ChatGroq(
+    groq_api_key=os.getenv("GROQ_API_KEY"),
+    model_name="llama3-8b-8192"
+)
+
+response = llm.invoke("What is the capital of France?")
+print(response.content)
